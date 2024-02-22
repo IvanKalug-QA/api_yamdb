b8ba695f4bc6e0f8556a8e687144cbbf499dd4de
Aleksandr-Talbutdinov <talbutav@yandex.ru>
2024-02-22 11:38:10 +0300

    Add models: Category, Genre, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+from reviews.validators import validate_year
+
+
+class Category(models.Model):
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField('Слаг', max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField('Слаг', max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=256)
+    year = models.IntegerField('Год выпуска', validators=[validate_year])
+    description = models.TextField('Описание', blank=True, default=None)
+    genre = models.ManyToManyField(Genre, through='GenreTitle',
+                                   verbose_name='Жанр')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 null=True, verbose_name='Категория')
+
+    class Meta:
+        default_related_name = 'titles'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'category'],
+                name='unique_name_category',
+            )
+        ]
+
+    class Meta:
+        verbose_name = 'произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.genre} {self.title}'
