63c7974702de5fc8b073537c4cd85a16a4fa0808
Aleksandr-Talbutdinov <talbutav@yandex.ru>
2024-02-22 11:43:43 +0300

    Add function: validate_year

---
+++ api_yamdb/reviews/validators.py
@@ -0,0 +1,12 @@
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
+def validate_year(value):
+    """Валидация года выпуска произведения."""
+    this_year = timezone.now().year
+    if value > this_year:
+        raise ValidationError(
+            f'Год выпуска произведения не может '
+            f'быть больше {this_year}'
+        )
