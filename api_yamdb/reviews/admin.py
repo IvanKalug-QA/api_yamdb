58d8055ff3bc400aaf31581318942edf9523f462
Aleksandr-Talbutdinov <talbutav@yandex.ru>
2024-02-24 00:19:39 +0300

    Add models in admin site

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Genre, GenreTitle, Title
+
+
+class GenreTitleInline(admin.TabularInline):
+    model = GenreTitle
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+
+    @admin.display(description='Жанр')
+    def genre_display(self, obj):
+        """Функция для отображения ManyToManyField в list_display"""
+        return [genre.name for genre in obj.genre.all()]
+
+    list_display = ('name', 'year', 'category', 'genre_display')
+    inlines = (GenreTitleInline,)
+    search_fields = ('name',)
+    list_filter = ('category', 'year', 'genre')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -31,31 +31,33 @@ class Title(models.Model):
     name = models.CharField('Название', max_length=256)
     year = models.IntegerField('Год выпуска', validators=[validate_year])
     description = models.TextField('Описание', blank=True, default=None)
-    genre = models.ManyToManyField(Genre, through='GenreTitle',
-                                   verbose_name='Жанр')
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
                                  null=True, verbose_name='Категория')
 
     class Meta:
         default_related_name = 'titles'
-        constraints = [
-            models.UniqueConstraint(
-                fields=['name', 'category'],
-                name='unique_name_category',
-            )
-        ]
-
-    class Meta:
         verbose_name = 'произведение'
         verbose_name_plural = 'Произведения'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'year', 'category'],
+                name='unique_name_year_category',
+            )
+        ]
 
     def __str__(self):
         return self.name
 
 
 class GenreTitle(models.Model):
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE,
+                              verbose_name='Жанр')
     title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'жанр произведения'
+        verbose_name_plural = 'Жанры произведения'
 
     def __str__(self):
         return f'{self.genre} {self.title}'
