ac8e201c2a083728e26e82eec20c8c473278aad4
IVAN-GUY <kaluginivan2002@mail.ru>
2024-02-25 13:28:31 +0300

    fix user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 ROOT = (
     ('user', 'user'),
@@ -9,7 +9,30 @@ ROOT = (
 )
 
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, role='user',
+                    bio=None):
+        email = self.normalize_email(email)
+        user = self.model(
+            username=username, email=email, role=role
+        )
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, role='admin',
+                         bio=None):
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            username=username,
+            role=role,
+            is_superuser=1
+        )
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
     email = models.EmailField(max_length=254, unique=True)
     username = models.CharField(
         unique=True,
@@ -22,11 +45,16 @@ class User(AbstractUser):
             )
         ]
     )
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
     bio = models.TextField(blank=True)
     role = models.CharField(default='user',
                             blank=True, max_length=25, choices=ROOT)
+    is_superuser = models.IntegerField(max_length=1, default=0, blank=True)
     password = None
     last_login = None
+
+    objects = UserManager()
 
     REQUIRED_FIELDS = ['email']
 
